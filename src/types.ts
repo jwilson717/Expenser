9ea50fcfd64374583768e2eb832a9722cc001f97
= <jadenwilson717@gmail.com>
2021-02-26 14:39:24 -0700

    added types

--- src/types.ts
+++ src/types.ts
@@ -27,3 +27,20 @@ export interface userState {
    user: user|null;
    loading: boolean;
 }
+
+export interface accountType {
+   id: number;
+   type: string;
+}
+
+export interface account {
+   id: number;
+   type: accountType;
+   description: string;
+   balance: number;
+   userId: number;
+}
+
+export interface accounts {
+   accounts: account[];
+}
